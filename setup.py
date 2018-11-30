9145e298b8641a208d053b6131d4f33b4defb4c8
Sven Grübel <sven.gruebel@gmx.ch>
2018-11-30 10:46:33 +0100

    Update folder structure to pypi standard

---
+++ setup.py
@@ -0,0 +1,52 @@
+# coding=utf-8
+#
+# MIT License
+#
+# Copyright (c) 2018 Sven Grübel
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="poro-fetcher",
+    version="0.1.0",
+    author="Sven Grübel",
+    author_email="sven.gruebel@gmx.ch",
+    description="Python wrapper for the League of Legends API with rate limiting",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url="https://github.com/getpastthemonkey/poro-fetcher",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Topic :: Games/Entertainment",
+        "Topic :: Games/Entertainment :: Real Time Strategy",
+        "Topic :: Games/Entertainment :: Role-Playing",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    license="MIT"
+)
